4fe715c950926dcf562db43ffb072aad0fbcb53f
ValentinoXRL8 <20243ds024@utez.edu.mx>
2025-09-27 14:42:31 -0600

    Agregue el swich(pero aun no funciona)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner;
+
     public static void main(String[] args) {
         System.out.println("=== LABERINTO ===\n Reglas del juego: \n");
         System.out.println("Usa las teclas para poder moverte entre el laberinto y llegar a la meta");
@@ -21,19 +23,34 @@ public class Main {
         };
 
         MapaPrinter mapaPrinter = new MapaPrinter();
-        mapaPrinter.printMapa(mapa);
 
         int fila = 1;
         int columna = 2;
         mapa[fila][columna] = '@';
 
-        mapa[fila][columna] = ' ';
-        columna++;
-        mapa[fila][columna] = '@';
+        boolean Laberinto = true;
 
-        mapa[fila][columna] = ' ';
-        fila++;
-        mapa[fila][columna] = '@';
+        while (Laberinto) {
+            mapaPrinter.printMapa(mapa);
+            String movimiento = scanner.nextLine().toUpperCase();
+
+            int nuevafila = fila;
+            int nuevacolumna = columna;
+
+            switch (movimiento) {
+                case "W": nuevafila--;
+                break;
+                case "S": nuevafila++;
+                break;
+                case "A": nuevafila--;
+                break;
+                case "D": nuevafila++;
+                break;
+                default:
+                    System.out.println("Solo puedes usar las teclas del juego");
+                    continue;
+            }
+        }
 
         mapaPrinter.printMapa(mapa);
     }
